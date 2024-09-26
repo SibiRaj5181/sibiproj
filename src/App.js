2eda5a38de3a994883a4980c87528515e372755d
Sivaraj Sibi <sivarajsibi03@gmail.com>
2024-09-26 20:32:17 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Header from './components/Header'
+import './assests/css/style.css'
+
+export default function App() {
+  return (
+    <div>
+      <Header/>
+    </div>
+  )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+export default function Header() {
+    return (
+        <header>
+     <nav>
+        <div class="nav-links">
+            <a href="#home">home</a>
+            <a href="#band">band</a>
+            <a href="#tour">tour</a>
+            <a href="#contract">contact</a>
+        
+
+        </div>
+        <div>
+            <i class="fa fa-search"></i>
+        </div>
+     </nav>
+    </header>
+    )
+}
